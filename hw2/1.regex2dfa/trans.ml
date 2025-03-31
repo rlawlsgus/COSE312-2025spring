f43ad8e51f5a9e7c3012b9cfcc1a8bc6e858fbe1
Hakjoo Oh <hakjoo_oh@korea.ac.kr>
2025-03-31 16:55:01 +0900

    add hw2

---
+++ hw2/1.regex2dfa/trans.ml
@@ -0,0 +1,19 @@
+open Regex 
+
+exception Not_implemented
+
+let regex2nfa : Regex.t -> Nfa.t 
+=fun _ -> raise Not_implemented (* TODO *)
+
+let nfa2dfa : Nfa.t -> Dfa.t
+=fun _ -> raise Not_implemented (* TODO *)
+ 
+(* Do not modify this function *)
+let regex2dfa : Regex.t -> Dfa.t
+=fun regex -> 
+  let nfa = regex2nfa regex in
+  let dfa = nfa2dfa nfa in
+    dfa
+
+let run_dfa : Dfa.t -> alphabet list -> bool
+=fun _ _ -> raise Not_implemented (* TODO *)
